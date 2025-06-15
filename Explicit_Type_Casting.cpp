7d09666862084f7d9676aec6de913a1a86ea53c8
Aditya J Parida <126658487+adigithu@users.noreply.github.com>
2025-06-15 16:35:05 +0530

    Add files via upload
    Explicit Type Casting

---
+++ Explicit_Type_Casting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int var=25;
+    float var1=45.78;
+    cout<<"intvar="<<var<<endl;
+    cout<<"floatvar="<<var1<<endl;
+    cout<<"float(var)="<<float(var)<<endl;
+    cout<<"int(var)="<<int(var)<<endl;
+    return 1;
+}
