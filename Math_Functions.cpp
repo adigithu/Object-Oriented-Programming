040426be545d5335d41db8d5ee7e2d7d190e8f87
Aditya J Parida <126658487+adigithu@users.noreply.github.com>
2025-06-23 20:42:10 +0530

    Add files via upload
    Use of math functions

---
+++ Math_Functions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<fixed<<setprecision(2);
+    cout<<"sin(100)="<<sin(100.00)<<endl;
+    cout<<"cos(100)="<<cos(100.00)<<endl;
+    cout<<"tan(100)="<<tan(100.00)<<endl;
+    cout<<"7 to the power 6 is "<<pow(7.0,6.0)<<endl;
+    cout<<"log10(10)="<<log10(10.00)<<endl;
+    cout<<"sqrt(2)="<<sqrt(2.00)<<endl;
+    return 0;
+}
